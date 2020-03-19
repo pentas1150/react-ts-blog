5a395cc527eb4273761a9bb07f3aea3b20c8826b
pentas1150 <beewol@soongsil.ac.kr>
2020-03-19 16:40:42 +0900

    1)블로그 포스트 양식 수정 2)댓글 Form 추가

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,21 +1,37 @@
 import React, { useState, useEffect } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "../modules/index";
+import { useDispatch } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 import axios from "axios";
-import { Table } from "react-bootstrap";
+import { Card, Form, Button } from "react-bootstrap";
 import { removeInfo } from "../modules/LoginInfo";
+import renderHTML from "react-render-html";
 
 function Post() {
-  const [post, setPost] = useState({ title: "", content: "" });
+  const [post, setPost] = useState({ id: 0, title: "", content: "", author: "", createdAt: "" });
+  const [commentAuthor, setCommentAuthor] = useState("");
+  const [comment, setComment] = useState("");
   const { postId } = useParams();
-  const session = useSelector((state: RootState) => state.LoginInfo.session);
   const history = useHistory();
   const dispatch = useDispatch();
 
+  const onChangeComment = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setComment(e.target.value);
+  };
+  const onChangeCommentAuthor = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCommentAuthor(e.target.value);
+  };
+  const onSubmitComment = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    alert(`${commentAuthor}: ${comment}`);
+
+    setCommentAuthor("");
+    setComment("");
+  };
+
   useEffect(() => {
     const getPost = async () => {
-      const result = await axios.get(`http://192.168.0.10:8080/post/${postId}`, { headers: session });
+      const result = await axios.get(`http://192.168.0.10:8080/post/${postId}`);
       if (!result.data.authenticated) {
         dispatch(removeInfo());
 
@@ -30,14 +46,35 @@ function Post() {
   }, []);
 
   return (
-    <Table responsive>
-      <thead>
-        <th>{post.title}</th>
-      </thead>
-      <tr>
-        <td>{post.content}</td>
-      </tr>
-    </Table>
+    <div>
+      <h1 className="mt-4">{post.title}</h1>
+      <p className="lead">by {post.author}</p>
+      <hr />
+      <p>Posted on {post.createdAt}</p>
+      <hr />
+      <div>{renderHTML(post.content)}</div>
+      <hr />
+      <Card>
+        <Card.Header>
+          <h5>Leave a Comment</h5>
+        </Card.Header>
+        <Card.Body>
+          <Form onSubmit={onSubmitComment}>
+            <Form.Group controlId="comment-author">
+              <Form.Label>Name</Form.Label>
+              <Form.Control type="text" onChange={onChangeCommentAuthor} value={commentAuthor} />
+            </Form.Group>
+            <Form.Group controlId="comment">
+              <Form.Label>Comment</Form.Label>
+              <Form.Control type="text" onChange={onChangeComment} value={comment} />
+            </Form.Group>
+            <Button type="submit" variant="secondary">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
